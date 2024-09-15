83aa65186284609afe3a2eb2e6a3af92860a64e9
tannershartel <tanner.shartel@gmail.com>
2024-09-15 12:57:47 -0700

    debug.s answered

--- debug.s
+++ debug.s
@@ -2,6 +2,8 @@
 #  Max Score: 9 points
 #
 # Students: 
+# Tanner Shartel (50% effort)
+# Katie Dionne (50% effort)
 #
 # 'count_occurence.a' - count the occurrences of a specific character in string 
 # "str". Indexed addressing is used to access the array elements.
@@ -12,8 +14,19 @@
 #
 # Questions:-
 # 1. Briefly describe the purposes of the registers, $t0, $t1, $t2, and $t3.
+#
+# $t0 is the temporary register that stores the value of each character from the 
+# string. $t1 is the incremented index that traverses the string. $t2 stores the # number of instances of the desired character. $t3 stores the character that is 
+# being counted in the string.
+#
 # 2. Currently, the program is stuck in an infinite loop. Make use of 
 #    breakpoints to locate, and correct the error.
+#
+#The error is in the con: branch, in the add instruction. This instruction is 
+# used to traverse the string, so it needs to increment $t1 by 1 each time it
+# loops. It incorrectly added the value stored in $t2 by 1, and stored that in 
+# $t1, so $t1 stayed at 1 indefinitely.
+#
 
 	.text
 
@@ -34,7 +47,7 @@ loop:
 
 con:	
 
-    add     $t1, $t2, 1	    # increase indexing register $t1
+    add     $t1, $t1, 1	    # increase indexing register $t1 ERROR HERE, incorrect increment
 	j       loop	       	# continues the loop
 
 strEnd:
